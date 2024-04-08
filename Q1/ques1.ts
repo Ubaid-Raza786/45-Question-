f462c3532d98034b534c76d5559ada6e578688a1
Ubaid Raza <163852784+Ubaid-Raza786@users.noreply.github.com>
2024-04-08 03:00:35 -0700

    First commit

---
+++ Q1/ques1.ts
@@ -0,0 +1,2 @@
+//Already Install Node.js, TypeScript and VS Code on in my computer.
+
---
+++ Q4/ques4.js
@@ -0,0 +1,3 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, "A person who never made a mistake never tried anything."
+console.log("Muhammad Ali Jinnah once said,'There are two powers in the world; one is the sword and the other is the pen.'");
